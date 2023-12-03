599171dc4fd131ac550940d425a1527e7f50424b
Ahmed-M-Sadek <ahmedms.ftw@gmail..com>
2023-12-03 08:51:42 +0200

    qt rebase

--- model.py
+++ model.py
@@ -1,10 +1,14 @@
-from taipy.gui import Gui
-from tensorflow.keras import models
-from PIL import Image
+import sys
+import os
+from PyQt5 import QtGui
+from PyQt5.QtWidgets import (
+    QApplication, QDesktopWidget, QHBoxLayout, QLabel, QPushButton, QVBoxLayout, QGridLayout,
+    QWidget, QTextEdit
+)
+from PyQt5.QtGui import QPixmap, QCursor, QIcon, QColor,QImage
+from PyQt5.QtCore import pyqtSignal, pyqtSlot, Qt, QThread
 import numpy as np
 import cv2 as cv
-import time
-
 class_names = {
     0: 'angry',
     1: 'disgusted',
@@ -14,144 +18,83 @@ class_names = {
     5: 'sad',
     6: 'surprised',
 }
-colors = {
-    'angry': (0, 0, 255),       
-    'disgusted': (0, 255, 0),   
-    'fearful': (255, 0, 255),   
-    'happy': (255, 255, 0),     
-    'neutral': (128, 128, 128), 
-    'sad': (0, 128, 255),       
-    'surprised': (0, 255, 255)  
-}
-haar = True
-if haar:
-    model = models.load_model("emotion_detection_model_with_face_detection.keras")
-else:
-    model = models.load_model("emotion_detection_model.keras")
+class VideoThread(QThread):
+    change_pixmap_signal = pyqtSignal(np.ndarray)
 
-def predict_mood(model,path_to_img):
-    img = Image.open(path_to_img)
-    img = img.convert("RGB")
-    img = img.resize((48, 48))
-    data = np.asarray(img)
-    data = data / 255
-    probs = model.predict(np.array([data])[:1])
+    def __init__(self):
+        super().__init__()
+        self._run_flag = True
 
-    top_prob = probs.max()
-    top_pred = class_names[np.argmax(probs)]
+    def run(self):
+        # capture from web cam
+        cap = cv.VideoCapture(0)
+        while self._run_flag:
+            ret, cv_img = cap.read()
+            if ret:
+                self.change_pixmap_signal.emit(cv_img)
+        # shut down capture system
+        cap.release()
+
+    def stop(self):
+        """Sets run flag to False and waits for thread to finish"""
+        self._run_flag = False
+        self.wait()
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Emotion detection")
+        self.setFixedHeight(600)
+        self.setFixedWidth(900)
+        self.setWindowIcon("")
+
+        self.image_label = QLabel(self)
+        self.image_label.setAlignment(Qt.AlignCenter)
+        #'angry','disgusted','fearful''happy''neutral''sad''surprised'
+        self.angry_label = QLabel('angry')
+        self.disgusted_label = QLabel('disgusted')
+        self.fearful_label = QLabel('fearful')
+        self.happy_label = QLabel('happy')
+        self.neutral_label = QLabel('neutral')
+        self.sad_label = QLabel('sad')
+        self.surprised_label = QLabel('surprised')
+
+        layout=QGridLayout()
+        layout.addWidget(self.angry_label,0,0,12,12)
+        layout.addWidget(self.disgusted_label,2,13,1,4)
+        layout.addWidget(self.fearful_label,3,13,1,4)
+        layout.addWidget(self.happy_label,4,13,1,4)
+        layout.addWidget(self.neutral_label,5,13,1,4)
+        layout.addWidget(self.sad_label,6,13,1,4)
+        layout.addWidget(self.surprised_label,7,13,1,4)
+
+        self.setLayout(layout)
+        self.thread = VideoThread()
+        # connect its signal to the update_image slot
+        self.thread.change_pixmap_signal.connect(self.update_image)
+        # start the thread
+        self.thread.start()
+
+    def closeEvent(self, event):
+        self.thread.stop()
+        event.accept()
+
+    @pyqtSlot(np.ndarray)
+    def update_image(self, cv_img):
+        """Updates the image_label with a new opencv image"""
+        qt_img = self.convert_cv_qt(cv_img)
+        self.image_label.setPixmap(qt_img)
     
-    return top_prob, top_pred
+    def convert_cv_qt(self, cv_img):
+        """Convert from an opencv image to QPixmap"""
+        rgb_image = cv.cvtColor(cv_img, cv.COLOR_BGR2RGB)
+        h, w, ch = rgb_image.shape
+        bytes_per_line = ch * w
+        convert_to_Qt_format = QtGui.QImage(rgb_image.data, w, h, bytes_per_line, QtGui.QImage.Format_RGB888)
+        p = convert_to_Qt_format.scaled(self.image_label.size(), Qt.KeepAspectRatio)
+        return QPixmap.fromImage(p)
 
-
-face_cascade = cv.CascadeClassifier("haarcascade_frontalface_default.xml")
-
-
-img_path="placeholder.png"
-content=""
-pred=""
-prob=0
-captured_image = None
-value=""
-is_webcam_active=False
-index="""
-<|text-center|
-
-<|{"logo.png"}|image|width=30vw|>
-# Emotion : **<|{pred}|>**{: .color-primary; .font-size-h1}
-
-<|{content}|file_selector|extensions=.jpg,.jpeg,.gif,.png|>
-Select an image
-<br/>
-<|{img_path}|image|width=20vw|>
-
-<|{prob}|indicator|value={prob}|min=0|max=100|width=25vw|>
-
-<|{pred}|>
-<br/>
-<container|container|part|
-
-
-<br/>
-
-<card|card p-half|part|
-## **Webcam**{: .color-primary} component
-
-<|text-center|part|
-<|{value}|toggle|lov=Files;Webcam|>
-<|{is_webcam_active}|>
-|card>
-|container>
->
-"""
-def predict_mood_from_frame(frame):
-    img = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
-    img = cv.resize(img, (48, 48))
-    data = np.asarray(img) / 255
-    probs = model.predict(np.array([data]))
-
-    top_prob = probs.max()
-    top_pred = class_names[np.argmax(probs)]
-
-    return top_prob, top_pred
-
-def on_change(state, var_name, var_val):
-    if var_name == "content":
-        top_prob, top_pred = predict_mood(model, var_val)
-        state.prob = round(top_prob * 100)
-        state.pred = top_pred
-        state.img_path = var_val
-        img = cv.imread(state.img_path, cv.IMREAD_UNCHANGED)
-        gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-        faces = face_cascade.detectMultiScale(gray,minNeighbors=1)
-        for (x, y, w, h) in faces:
-            cv.rectangle(img, (x, y), (x + w, y + h), (255, 255, 0), 2)
-            cv.putText(img, top_pred, (x, y - 10), cv.FONT_HERSHEY_SIMPLEX, 0.9, colors[top_pred], 2)
-
-        state.img_path = cv.imencode(".jpg", img)[1].tobytes()
-    if var_name == "value":
-        if var_val=="Files":
-            state.img_path = "placeholder.png"
-            state.pred = ""
-            state.is_webcam_active = False
-        elif var_val == "Webcam":
-            state.is_webcam_active = True
-
-            while state.is_webcam_active:
-                cap = cv.VideoCapture(0)
-                ret, frame = cap.read()
-                state.refresh("img_path")
-
-                if frame.size != 0:
-                    img_path = cv.imencode(".jpg", frame)[1].tobytes()
-                    state.img_path = img_path
-                    state.refresh("img_path")
-                
-
-                if state.value == "Files":
-                    state.is_webcam_active = False
-                    break
-
-                if ret:
-                    top_prob, top_pred = predict_mood_from_frame(frame)
-                    state.prob = round(top_prob * 100)
-                    state.pred = "Mood: " + top_pred
-                    img = frame.copy()
-                    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-                    faces = face_cascade.detectMultiScale(gray, minNeighbors=1)
-                    for (x, y, w, h) in faces:
-                        cv.rectangle(img, (x, y), (x + w, y + h), (255, 255, 0), 2)
-                        cv.putText(img, top_pred, (x, y - 10), cv.FONT_HERSHEY_SIMPLEX, 0.9, colors[top_pred], 2)
-
-                    captured_image = img
-                    time.sleep(0.1)
-                cap.release()
-            state.img_path = "placeholder.png"
-            state.refresh("img_path")
-
-            state.is_webcam_active = False
-
-app = Gui(page=index)
 if __name__ == "__main__":
-
-    app.run(use_reloader=True, port=1050)
-        
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
